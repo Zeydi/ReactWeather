19d2ef940cd2b1173a6eacaa31134322fc951959
Zeydi <oussema.zeidi@ieee.org>
2018-02-15 11:02:52 +0100

    Add components and routing

---
+++ app/components/About.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const About = React.createClass({
+  render: function() {
+    return (
+      <h3>About</h3>
+    );
+  }
+});
+
+module.exports = About;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+const {Link} = require('react-router');
+
+
+const Nav = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h2>Nav Component</h2>
+        <Link to="/">Get Weather</Link>
+        <Link to="/about">About</Link>
+        <Link to="/examples">Examples</Link>
+      </div>
+    );
+
+  }
+});
+
+module.exports = Nav;
